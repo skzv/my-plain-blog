4907e2387fad234308513010e7b45f806fafe7bb
skz Kuznetsov <me@skz.dev>
2021-11-30 00:42:22 -0800

    add monte carlo generation and plotting

---
+++ _drafts/2021-11-29-monte-carlo-options-pricing.md
@@ -0,0 +1,16 @@
+---
+title: Monte Carlo Options Pricing
+updated: 2021-12-07 00:00
+imgpath: /assets/img/gradient-descent
+previewurl: /gradient-descent-preview.png
+---
+
+#### _The options casino_
+
+<div id='plot-0'></div>
+
+<button onclick="recompute()">Recompute</button>
+
+<script src='https://cdn.plot.ly/plotly-2.4.2.min.js'></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.5.1/math.js" integrity="sha512-AfRcJIj922x/jSJpQLnry0DYIBg6EGCtwk/MiQ6QvDlzb7kNFxH8EdqXLkaXXY3YHQS9FrSb8H7LzuLn0CZQ1A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="/assets/js/monte-carlo-options.js" async></script>
---
+++ assets/js/monte-carlo-options.js
@@ -0,0 +1,102 @@
+const TRADING_DAYS_IN_YEAR = 252;
+const dT = 1 / TRADING_DAYS_IN_YEAR;
+
+const RANGE_MARGIN = 0.000005;
+
+var s0 = 10;
+var mu_y = 0.1;
+var sigma_y = 0.3;
+var N = 252;
+var M = 5;
+
+var TS = generateMonteCarloPriceSeries(s0, mu_y, sigma_y, dT, N);
+console.log(TS);
+
+var data = [{
+    x: TS.t,
+    y: TS.s,
+    line: { simplify: false },
+}];
+var layout = {
+    title: "Monte Carlo Options",
+    autosize: true,
+    plot_bgcolor: "black",
+    paper_bgcolor: "rgba(0,0,0,0)",
+    "yaxis": {
+        "gridcolor": "grey",
+    },
+    "xaxis": {
+        "gridcolor": "grey",
+    },
+};
+
+var config = { responsive: true }
+
+Plotly.newPlot('plot-0', data, layout, config);
+
+function recompute() {
+    var TS = generateMonteCarloPriceSeries(s0, mu_y, sigma_y, dT, N);
+    var data = [{
+        x: TS.t,
+        y: TS.s,
+        line: { simplify: false },
+    }];
+
+    var min = math.min(TS.s);
+    var max = math.max(TS.s);
+
+    Plotly.animate('plot-0', {
+        data: data,
+        traces: [0],
+        layout: {
+        }
+    }, {
+        transition: {
+            duration: 500,
+            easing: 'cubic-in-out'
+        },
+        frame: {
+            duration: 500
+        }
+    });
+
+    Plotly.animate('plot-0', {
+        layout: {
+            yaxis: { range: [min - RANGE_MARGIN * min, max + RANGE_MARGIN * max] }
+        }
+    }, {
+        transition: {
+            duration: 500,
+            easing: 'cubic-in-out'
+        },
+        frame: {
+            duration: 500
+        }
+    });
+}
+
+function generateMonteCarloPriceSeries(s0, mu_y, sigma_y, dT, N) {
+    var ts = generateDailyMonteCarloReturns(mu_y, sigma_y, dT, N);
+    console.log(ts);
+    var cum_r = accumulateMonteCarloReturns(ts.r);
+    var S = math.multiply(s0, math.exp(cum_r));
+    return { t: ts.t, s: S };
+}
+
+function accumulateMonteCarloReturns(r) {
+    var cumsum = 0;
+    return r.map(s => cumsum += s);
+}
+
+function generateDailyMonteCarloReturns(mu_y, sigma_y, dT, N) {
+    var mu_d = mu_y / TRADING_DAYS_IN_YEAR;
+    var sigma_d = sigma_y / TRADING_DAYS_IN_YEAR;
+
+    var z = math.matrix(math.random([1, N], -0.5, 0.5));
+    var means = math.multiply(math.ones([1, N]), mu_d * dT);
+    var impulses = math.multiply(z, sigma_d * math.sqrt(dT));
+    var r = [0].concat(math.add(means, impulses).toArray()[0]);
+
+    var t = [0].concat(math.range(1, N, true).toArray());
+    return { t: t, r: r };
+}
